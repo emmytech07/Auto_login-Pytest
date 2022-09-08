75facd9ab064c1aa2544cf0ae1dd4f4fe397b9a4
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-08 01:01:20 +0100

    Automated web page using test_fixtures

---
+++ test_fixtures.py
@@ -0,0 +1,39 @@
+"""
+precondition
+    setup, Connection API
+
+Test
+Test
+Assertion
+Postcon
+    clean, close, close
+
+"""
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver= None
+
+@pytest.fixture
+def setup():
+    global driver
+    print("Start Browser")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    yield
+    driver.quit()
+    print("Close Browser")
+
+def test_1(setup):
+    driver.get("https://www.facebook.com")
+    print("Test 1 executed")
+    
+
+def test_2(setup):
+    driver.get("https://www.google.com")
+    print("Test 2 executed")
+    
+
+def test_3(setup):
+    driver.get("https://www.instagram.com")
+    print("Test 3 executed")
+    
